23c91465000453bece11e85bb048381455bcdc7d
Ren <cassandra.chen@uoit.net>
2022-01-03 20:41:50 +0000

    updated tables and fixed issues with table creation

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,16 +1,16 @@
-CREATE TABLE assignments(
+CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
-  name varchar(255),
-  content text,
-  day DATE,
-  chapter int,
-  duration TIME
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
 );
 
-CREATE TABLE assignment_submission(
+CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration int,
+  duration INTEGER,
   submission_date DATE
-)
+);
